921420ed598c1ed142d66ae7fc317dbd7ce5ccd7
Mr-Bellali <bellali.yassine52@gmail.com>
2024-03-12 10:49:29 +0000

    decryptage

--- cryptage.c
+++ cryptage.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char * decryptage (char *input) {
+    char *decrypted = (char*) malloc(strlen(input) + 1);
+
+    if (decrypted == NULL){
+        printf("Memory allocation failed\n");
+    }
+
+
+
+    for(int i=0; i < input[i] != '\0'; i++){
+        decrypted [i] = input[i] - 3;
+    }
+    decrypted[strlen(input)] = '\0';
+
+    return decrypted;
+}
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    char *input;
+    printf("entrer crypted value: ");
+    scanf("%s",input);
+
+    char *decrypted = decryptage(input);
+
+    printf("decrypted value: %s",decrypted);
+
+    free(decrypted);
     return 0;
 }
